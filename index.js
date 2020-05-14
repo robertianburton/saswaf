5200abae367ce323b2a4832cdbfd4935c4ba9d03
Robert Burton <trumpetsandterminals@gmail.com>
2020-05-13 23:39:25 -0400

    Added chat system

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const path = require('path');
+const socket = require("socket.io");
 const PORT = process.env.PORT || 5000;
 
-express()
+const app = express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
 	.get('/soundcheck', (req, res) => res.render('pages/soundcheck'))
-	.get('/chat', (req, res) => res.render('pages/chat'))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.get('/chat', (req, res) => res.render('pages/chat'));
+
+const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socket(server);
+
+const activeUsers = new Set();
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+
+  socket.on("new user", function (data) {
+  	socket.userId = data;
+  	activeUsers.add(data);
+  	io.emit("new user", [...activeUsers]);
+  	console.log("New user. Added: " + data);
+  });
+
+  socket.on("chat message", function (data) {
+  	io.emit("chat message", data);
+  	console.log("New message from "+ data.sender);
+  });
+
+  socket.on("disconnect", () => {
+  	activeUsers.delete(socket.userId);
+  	io.emit("user disconnected", socket.userId);
+  })
+});
---
+++ public/js/chat.js
@@ -0,0 +1,67 @@
+(function() {
+
+    var counter = 0;
+    var userNameElem = null;
+    var socket = null;
+    var chatLogElem = null;
+    var chatTextBoxElem = null;
+    var chatSubmitElem = null;
+    let userName = "";
+
+    function startup() {
+        console.log("Chat JS Starting Up...");
+        socket = io();
+        
+        userNameElem = document.getElementById('userNameElem');
+        chatLogElem = document.getElementById('chatLogElem');
+        chatTextBoxElem = document.getElementById('chatTextBoxElem');
+        chatSubmitElem = document.getElementById('chatSubmitElem');
+
+        newUserConnection();
+
+        socket.on("new user", function(data) {
+            userListChange(data);
+            scroll();
+        });
+
+        socket.on("chat message", function(data) {
+            addNewMessage(data);
+            scroll();
+        });
+
+        chatSubmitElem.addEventListener('click', function(ev){
+            ev.preventDefault();
+            socket.emit("chat message", {
+                message: chatTextBoxElem.value,
+                sender: userName,
+            });
+            chatTextBoxElem.value="";
+            chatTextBoxElem.focus();
+        }, false);
+
+        console.log("Chat JS Startup Complete.");
+    };
+
+    const newUserConnection = (user) => {
+        userName = user || `User${Math.floor(Math.random() * 1000000)}`;
+        socket.emit("new user", userName);
+        console.log("Assigned Username: " + userName);
+        userNameElem.innerHTML = userName;
+    };
+
+    const userListChange = (userNameList) => {
+        chatLogElem.innerHTML += `<p><span class="font-weight-bold">Server: </span>User List Change! Users: ` 
+        + userNameList.join(', ')
+        + `</p>`;
+    };
+
+    const addNewMessage = (data) => {
+        chatLogElem.innerHTML += `<p><span class="font-weight-bold">${data.sender}: </span>${data.message}</p>`
+    };
+
+    function scroll() {
+        $('#chatLogElem').scrollTop($('#chatLogElem')[0].scrollHeight)
+    };
+    
+    window.addEventListener('load', startup, false);
+})();
