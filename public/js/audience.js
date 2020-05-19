3da3f0adc77bd0cf5b64fc3d78f2a38927b3cfc3
Robert Burton <trumpetsandterminals@gmail.com>
2020-05-19 00:47:57 -0400

    Trying Xirsys for STUN/TURN

--- public/js/audience.js
+++ public/js/audience.js
@@ -59,12 +59,7 @@
     const configurationA = {
         iceServers: [{urls: [
         'stun:stun.robertianburton.com:3478'
-    ]},
-    {
-      'url': 'turn:turn.45.55.43.60:3478?transport=udp',
-      'credential': 'testpassword',
-      'username': 'testuser'
-    },]};
+    ]}]};
     const configurationB = {
         iceServers: [{urls: [
         'stun:stun.l.google.com:19302',
@@ -72,7 +67,18 @@
         'stun:stun2.l.google.com:19302',
         'stun:stun.l.google.com:19302?transport=udp',
     ]}]};
-    const configuration = configurationA;
+    const configurationC = {
+        iceServers: [{   urls: [ "stun:us-turn2.xirsys.com" ]}, {   username: 
+            "k3IAtn2K1yMCrpypkP_CJCyEV7m3FHThFwcUnIxp_4i8-ZuFR4JQN0zqjllYFBXYAAAAAF7DZDF5YWtldHlTYXhlcw==",  
+             credential: "6f541688-998b-11ea-8e17-0242ac140004",   urls: [   
+            "turn:us-turn2.xirsys.com:80?transport=udp",    
+            "turn:us-turn2.xirsys.com:3478?transport=udp", 
+            "turn:us-turn2.xirsys.com:80?transport=tcp",     
+            "turn:us-turn2.xirsys.com:3478?transport=tcp", 
+            "turns:us-turn2.xirsys.com:443?transport=tcp",      
+         "turns:us-turn2.xirsys.com:5349?transport=tcp"   ]}]
+};
+    const configuration = configurationC;
     const pc = new RTCPeerConnection(configuration);
 
     // send any ice candidates to the other peer
