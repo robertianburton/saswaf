14fe1cbf6d559661ab13b7d919e0cdd3aa769851
Robert Burton <trumpetsandterminals@gmail.com>
2020-05-22 23:25:41 -0400

    More TURN changes

--- public/js/equal.js
+++ public/js/equal.js
@@ -78,7 +78,9 @@
                 username: "testuser",
                 credential: "testpassword",
                 urls: [
-                    "turn:turn.robertianburton.com:3478"
+                    "turn:turn.robertianburton.com:3478",
+                    "turn:turn.robertianburton.com:3478?transport=udp",
+                    "turn:turn.robertianburton.com:3478?transport=tcp"
                 ]
             }
     ]};
