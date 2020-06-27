371c66656346ed51021d6128d48e9e066f8e0734
Robert Burton <trumpetsandterminals@gmail.com>
2020-06-27 02:38:32 -0400

    Laid groundwork for supporting both getDisplayMedia and getUserMedia simultaneously.

--- public/js/host.js
+++ public/js/host.js
@@ -24,28 +24,28 @@
         videoLocalElem = document.getElementById('videoLocalElem');
 
         buttonVideoSizeSource = document.getElementById('buttonVideoSizeSource');
-        buttonVideoSizeSource.addEventListener('click', function(ev){
+        buttonVideoSizeSource.addEventListener('click', function(ev) {
             videoLocalElem.style.width = "auto";
             videoLocalElem.scrollIntoView();
             ev.preventDefault();
         }, false);
 
         buttonVideoSizeResponsive = document.getElementById('buttonVideoSizeResponsive');
-        buttonVideoSizeResponsive.addEventListener('click', function(ev){
+        buttonVideoSizeResponsive.addEventListener('click', function(ev) {
             videoLocalElem.style.width = "100%";
 
             ev.preventDefault();
         }, false);
 
         buttonVideoSizePage = document.getElementById('buttonVideoSizePage');
-        buttonVideoSizePage.addEventListener('click', function(ev){
+        buttonVideoSizePage.addEventListener('click', function(ev) {
             videoLocalElem.style.width = window.innerWidth;
 
-            var docH = $( document ).height();
+            var docH = $(document).height();
             var vidH = $('#videoElem').height();
             var videoScale = ($('#videoElem').width() / $('#videoLocalElem').height());
-            var topH = docH-vidH;
-            var workableH = Math.floor((window.innerHeight-topH-0)*videoScale);
+            var topH = docH - vidH;
+            var workableH = Math.floor((window.innerHeight - topH - 0) * videoScale);
 
             videoLocalElem.style.width = workableH + "px";
             videoLocalElem.scrollIntoView();
@@ -53,32 +53,34 @@
         }, false);
 
         buttonStart = document.getElementById('buttonStart');
-        buttonStart.addEventListener('click', function(ev){
+        buttonStart.addEventListener('click', function(ev) {
             console.log("Start Button");
             start();
             ev.preventDefault();
         }, false);
 
         buttonLogConnection = document.getElementById('buttonLogConnection');
-        buttonLogConnection.addEventListener('click', function(ev){
+        buttonLogConnection.addEventListener('click', function(ev) {
             console.log("Log Connection");
             console.log(pc);
             console.log(signaling.id);
-            
+            if (stream) {
+                console.log(stream);
+            };
             ev.preventDefault();
         }, false);
 
         userIdField = document.getElementById('userIdField');
-        
+
         friendListItems = document.getElementById('friendListItems');
 
-        
-        
+
+
 
         console.log("Host JS Startup Complete.");
     }
 
-    
+
     const constraints = {
         video: {
             width: {
@@ -92,7 +94,7 @@
             }
         },
         audio: {
-            'channelCount': {'ideal': 2},
+            'channelCount': { 'ideal': 2 },
             'echoCancellation': false,
             'autoGainControl': false,
             'googAutoGainControl': false,
@@ -101,9 +103,21 @@
             'sampleSize': 16
         }
     };
+    var audioConstraints = {
+        video: false,
+        audio: {
+            'channelCount': { 'min': 2 },
+            'echoCancellation': false,
+            'autoGainControl': false,
+            'googAutoGainControl': false,
+            'noiseSuppression': false,
+            'sampleRate': 44100,
+            'sampleSize': 16
+
+        }
+    };
     const configurationA = {
-        iceServers: [
-            {
+        iceServers: [{
                 urls: ['stun:stun.robertianburton.com:3478']
             },
             {
@@ -115,89 +129,93 @@
                     "turn:turn.robertianburton.com:3478?transport=tcp"
                 ]
             }
-    ]};
+        ]
+    };
     const configurationB = {
-        iceServers: [{urls: [
-        'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun.l.google.com:19302?transport=udp',
-    ]}]};
+        iceServers: [{
+            urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun.l.google.com:19302?transport=udp',
+            ]
+        }]
+    };
     const configuration = configurationA;
-    
+
     function formatDate(date, format) {
         date = date.toJSON().split(/[:/.TZ-]/);
-        return format.replace(/[ymdhisu]/g, function (letter) {
+        return format.replace(/[ymdhisu]/g, function(letter) {
             return date['ymdhisu'.indexOf(letter)];
         });
     };
 
     function printToConsole(data) {
-        console.log(formatDate(new Date(), 'ymd hisu')+" "+JSON.stringify(data));
+        console.log(formatDate(new Date(), 'ymd hisu') + " " + JSON.stringify(data));
     };
 
     function sendToUser(data) {
         console.log(data);
-        signaling.emit("signalToUser",data);
+        signaling.emit("signalToUser", data);
     };
 
     function sendToServer(data) {
-        signaling.emit("signalToServer",data);
+        signaling.emit("signalToServer", data);
     };
 
     function sendAddHostToServer() {
         /*userName = user || `User${Math.floor(Math.random() * 1000000)}`;*/
-        sendToServer({'type':'addHost', 'id':signaling.id});
+        sendToServer({ 'type': 'addHost', 'id': signaling.id });
     };
 
     function fillFriendList() {
         friendListItems.innerHTML = '';
-        if(friendList.size>0) {
+        if (friendList.size > 0) {
             friendList.forEach(
                 (friend) => {
-                     friendListItems.innerHTML +='<button type="button" class="list-group-item list-group-item-action">'+friend+'</button>';
+                    friendListItems.innerHTML += '<button type="button" class="list-group-item list-group-item-action">' + friend + '</button>';
                 }
             );
         }
     };
 
     function bindSignalingHandlers(signalingObject) {
-        signalingObject.on("connect", async (data) =>  {
+        signalingObject.on("connect", async (data) => {
             console.log("Socket ID: " + signaling.id);
             userIdField = document.getElementById('userIdField');
-            userIdField.innerHTML=": "+signaling.id;
+            userIdField.innerHTML = ": " + signaling.id;
             sendAddHostToServer();
         });
 
-        signalingObject.on("signalToUser", async (data) =>  {
+        signalingObject.on("signalToUser", async (data) => {
             printToConsole("SignalToUser From " + data.fromId + " to " + data.toId + ":");
             console.log(data);
-            if(data.type==="newFriend") {
+            if (data.type === "newFriend") {
                 friendList.add(data.fromId);
                 console.log(friendList);
                 fillFriendList();
-                if(!pclist[data.fromId]) {
+                if (!pclist[data.fromId]) {
                     pclist[data.fromId] = new RTCPeerConnection(configuration);
                     /*pclist[data.fromId].onnegotiationneeded = handleNegotiationNeededEvent;*/
 
-                    pclist[data.fromId].onnegotiationneeded = function () {
+                    pclist[data.fromId].onnegotiationneeded = function() {
                         printToConsole("handleNegotiationNeededEvent");
                         console.log(data.fromId);
                         pclist[data.fromId].createOffer().then(function(offer) {
-                            var processedOffer = processOfferForStereo(offer);
-                            console.log("PROCESSED OFFER NN");
-                            console.log(processedOffer);
-                            return pclist[data.fromId].setLocalDescription(processedOffer);
-                        })
-                        .then(function() {
-                            sendToUser({
-                                fromId: signaling.id,
-                                toId: data.fromId,
-                                type: "video-offer",
-                                sdp: pclist[data.fromId].localDescription
-                            });
-                        })
-                        .catch(reportError);
+                                var processedOffer = processOfferForStereo(offer);
+                                console.log("PROCESSED OFFER NN");
+                                console.log(processedOffer);
+                                return pclist[data.fromId].setLocalDescription(processedOffer);
+                            })
+                            .then(function() {
+                                sendToUser({
+                                    fromId: signaling.id,
+                                    toId: data.fromId,
+                                    type: "video-offer",
+                                    sdp: pclist[data.fromId].localDescription
+                                });
+                            })
+                            .catch(reportError);
                     };
 
 
@@ -206,14 +224,13 @@
                     /*pclist[data.fromId].oniceconnectionstatechange = onConnectionStateChange;*/
                     handleNegotiationNeededEvent(data.fromId);
                 }
-            }
-            else if(data.type==="video-answer") {
+            } else if (data.type === "video-answer") {
                 var desc = new RTCSessionDescription(data.sdp);
                 await pclist[data.fromId].setRemoteDescription(desc).catch(reportError);
                 /*console.log("In video-answer, printing pclist entry");
                 console.log(pclist[data.fromId]);*/
 
-                if(!pclist[data.fromId].nowStreaming) {
+                if (!pclist[data.fromId].nowStreaming) {
                     console.log("HAPPENING")
                     pclist[data.fromId].nowStreaming = 2;
                     stream.getTracks().forEach((track) => pclist[data.fromId].addTrack(track, stream));
@@ -225,16 +242,15 @@
                 } catch (err) {
                     console.error(err)
                 };*/
-            }
-            else if(data.type==="new-ice-candidate") {
+            } else if (data.type === "new-ice-candidate") {
                 handleNewICECandidate(data);
             };
         });
 
-        signalingObject.on("leaver", async (data) =>  {
+        signalingObject.on("leaver", async (data) => {
             console.log(data);
-            
-            if(pclist[data.fromId]) {
+
+            if (pclist[data.fromId]) {
                 const senders = pclist[data.fromId].getSenders();
                 senders.forEach((sender) => pclist[data.fromId].removeTrack(sender));
                 pclist[data.fromId].close();
@@ -246,7 +262,7 @@
     };
 
     function handleGetUserMediaError(e) {
-        switch(e.name) {
+        switch (e.name) {
             case "NotFoundError":
                 alert("Unable to open your call because no camera and/or microphone" +
                     "were found.");
@@ -267,20 +283,20 @@
         printToConsole("handleNegotiationNeededEvent");
         console.log(friendId);
         pclist[friendId].createOffer().then(function(offer) {
-            var processedOffer = processOfferForStereo(offer);
-            console.log("PROCESSED OFFER NN");
-            console.log(processedOffer);
-            return pclist[friendId].setLocalDescription(processedOffer);
-        })
-        .then(function() {
-            sendToUser({
-                fromId: signaling.id,
-                toId: friendId,
-                type: "video-offer",
-                sdp: pclist[friendId].localDescription
-            });
-        })
-        .catch(reportError);
+                var processedOffer = processOfferForStereo(offer);
+                console.log("PROCESSED OFFER NN");
+                console.log(processedOffer);
+                return pclist[friendId].setLocalDescription(processedOffer);
+            })
+            .then(function() {
+                sendToUser({
+                    fromId: signaling.id,
+                    toId: friendId,
+                    type: "video-offer",
+                    sdp: pclist[friendId].localDescription
+                });
+            })
+            .catch(reportError);
     };
 
     function handleICECandidateEvent(data) {
@@ -300,7 +316,7 @@
         var candidate = new RTCIceCandidate(data.candidate);
 
         await pclist[data.fromId].addIceCandidate(candidate)
-        .catch(reportError);
+            .catch(reportError);
     };
 
     function reportError(e) {
@@ -309,47 +325,63 @@
     };
 
     function onConnectionStateChange(event) {
-        switch(pc.connectionState) {
-            case "connected": 
+        switch (pc.connectionState) {
+            case "connected":
                 console.log("Connection Connected!");
-            // The connection has become fully connected
-            break;
+                // The connection has become fully connected
+                break;
             case "disconnected":
             case "failed":
-            // One or more transports has terminated unexpectedly or in an error
+                // One or more transports has terminated unexpectedly or in an error
                 console.log("Failed! Closing!");
                 shutdown();
-            break;
+                break;
             case "closed":
-            // The connection has been closed
+                // The connection has been closed
                 console.log("Closed! Closing!");
                 shutdown();
-            break;
+                break;
         };
-        if(pc.iceConnectionState == 'disconnected') {
+        if (pc.iceConnectionState == 'disconnected') {
             console.log('Disconnected. Closing.');
             shutdown();
         }
     };
 
     async function start() {
-        
+
         nowStreaming = 1;
-        if(nowStreaming===1) {
+        if (nowStreaming === 1) {
             nowStreaming = 2;
             /*stream = await navigator.mediaDevices.getUserMedia(constraints);*/
-            await navigator.mediaDevices.getDisplayMedia(constraints).then(function(getDisplayMediaResult) {
+            /*await navigator.mediaDevices.getDisplayMedia(constraints).then(function(getDisplayMediaResult) {
                 stream = getDisplayMediaResult;
-            }).catch(handleGetUserMediaError);
+            }).catch(handleGetUserMediaError);*/
             /*console.log("Capabilities:");*/
             /*console.log(stream.getVideoTracks()[0].getCapabilities());*/
             /*stream.getTracks().forEach((track) => pc.addTrack(track, stream));*/
+
+
+            var tracks = [];
+            await navigator.mediaDevices.getDisplayMedia(constraints).then(function(getDisplayMediaResult) {
+                tracks = tracks.concat(getDisplayMediaResult.getTracks());
+            }).catch(handleGetUserMediaError);
+
+
+
+            /*await navigator.mediaDevices.getUserMedia(audioConstraints).then(function(getUserMediaResult) {
+                tracks = tracks.concat(getUserMediaResult.getTracks());
+            }).catch(handleGetUserMediaError);
+
+*/
+            stream = new MediaStream(tracks);
+
             videoLocalElem.srcObject = stream;
             nowStreaming = 3;
         };
 
         signaling = io();
-        userIdField.innerHTML=": Waiting...";
+        userIdField.innerHTML = ": Waiting...";
         bindSignalingHandlers(signaling);
     };
 
