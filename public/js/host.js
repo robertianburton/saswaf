0dd9e9502005b5924b26481243a8b812b4852e96
Robert Burton <trumpetsandterminals@gmail.com>
2020-08-27 20:47:20 -0400

    Adding more stun

--- public/js/host.js
+++ public/js/host.js
@@ -165,7 +165,11 @@
     const configurationB = {
         iceServers: [{
             urls: [
-                'stun:stun.robertianburton.com:3478'
+                'stun:stun.robertianburton.com:3478',
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun.l.google.com:19302?transport=udp',
             ]
         }]
     };
@@ -472,7 +476,11 @@
     function setConfiguration(turnCredentials) {
         const configurationD = {
             iceServers: [{
-                    urls: ['stun:stun.robertianburton.com:3478']
+                    urls: ['stun:stun.robertianburton.com:3478',
+                    'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun.l.google.com:19302?transport=udp']
                 },
                 {
                     username: turnCredentials.username,
