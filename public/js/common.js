7a95b0b2ec6f2ccc2832c4d8fdc4c53d984ce463
Robert Burton <trumpetsandterminals@gmail.com>
2020-10-30 21:39:30 -0400

    Removed homepage dependency on jQuery

--- public/js/common.js
+++ public/js/common.js
@@ -1,15 +1,19 @@
 function setNavbar() {
-	var path = $(location).attr('pathname');
-	$( '.navbar-nav a' ).each(function() {
-	    var href = $(this).attr('href');
-	    if (path == href) {
-	        $(this).addClass('active');
-	    }
-	});
-}
 
-$( document ).ready(function() {
-    console.log( "Common JS Starting Up..." );
-    setNavbar();
-    console.log( "Common JS Startup Complete." );
+	var url = location.href.split("/");
+	var navLinks = document.getElementsByClassName("nav-link");
+	var i = 0;
+	var currentPage = url[url.length - 1];
+	for (i; i < navLinks.length; i++) {
+		var pageCode = navLinks[i].href.split("/");
+		if (pageCode[pageCode.length - 1] == currentPage) {
+			navLinks[i].className = navLinks[i].className + " active";
+		};
+	};
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	console.log("Common JS Starting Up...");
+	setNavbar();
+	console.log("Common JS Startup Complete.");
 });
