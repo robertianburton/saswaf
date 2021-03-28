9f30643abab1a8a3c538e035fffb6cc60faec9aa
Robert Burton <trumpetsandterminals@gmail.com>
2021-03-28 17:32:16 -0400

    Fixed the navbar Watch label highlighting when on on the Watch page - wasn't working with ? in the url before

--- public/js/common.js
+++ public/js/common.js
@@ -3,10 +3,14 @@ function setNavbar() {
 	var url = location.href.split("/");
 	var navLinks = document.getElementsByClassName("nav-link");
 	var i = 0;
-	var currentPage = url[url.length - 1];
+	var currentPage = url[url.length - 1].substring(0,url[url.length - 1].indexOf("?"));
+
+	if(url[url.length - 1].indexOf("?") < 0) {
+		currentPage = url[url.length - 1]
+	}
+
 	for (i; i < navLinks.length; i++) {
 		var pageCode = navLinks[i].href.split("/");
-		console.log("PageCode: " + pageCode);
 		if (pageCode[pageCode.length - 1] == currentPage) {
 			navLinks[i].className = navLinks[i].className + " active";
 		};
